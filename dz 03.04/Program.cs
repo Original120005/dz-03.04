de2ea96547056ebca697f921b9602ce5ba435d8d
Дима <@DESKTOP-TC925PR>
2023-04-05 00:14:30 +0300

    Add project files.

---
+++ dz 03.04/Program.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace dz03_04
+{
+    interface IStrategy
+    {
+        object Drive();
+    }
+
+    class Context
+    {
+        private IStrategy strategy;
+
+        public Context() { }
+        public Context(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public void SetStrategy(IStrategy strategy)
+        {
+            this.strategy = strategy;
+        }
+
+        public void DoSomeBusinessLogic()
+        {
+            var result = strategy.Drive();
+            Console.WriteLine(result);
+        }
+    }
+
+    class Bicycle : IStrategy
+    {
+        public object Drive()
+        {
+            return " bicycle";
+        }
+    }
+
+    class Bus : IStrategy
+    {
+        public object Drive()
+        {
+            return "bus";
+        }
+    }
+
+    class Taxi : IStrategy
+    {
+        public object Drive()
+        {
+            return "taxi";
+        }
+    }
+
+
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var context = new Context();
+            context.SetStrategy(new Bus());
+            context.DoSomeBusinessLogic();
+
+
+
+        }
+    }
+}
